1700fc309f9b40eb9d58972b342ab3fa54a8f2e2
Ignacio Juan Martín Benedetti <tranceway@gmail.com>
2014-06-05 23:12:38 -0300

    mejoras

--- program.ar_python/graficar.py
+++ program.ar_python/graficar.py
@@ -3,39 +3,26 @@ from pyx import *
 from pyx.graph import axis
 
 
-def graficar(nombre=None, func=None, puntos=20, max=None, min=None, estilo=None):
+def graficar(nombre=None, func=None, puntos=20, max=20, min=-20, estilo=None):
     if not nombre and not func:
         return
 
-    if not max:
-        max = 4
-
-    if not min:
-        min = -4
-
-
-    xgridpainter = axis.painter.regular(gridattrs=[])
-    ygridpainter = axis.painter.regular(gridattrs=[attr.changelist([style.linestyle.dashed, None])])
+    gridpainter = axis.painter.regular(gridattrs=[attr.changelist([style.linestyle.dashed, None])])
 
     g = graph.graphxy(width=10,
-                      x=axis.lin(painter=xgridpainter,
-                                 #divisor=pi,
-                                 #texter=axis.texter.rational(suffix=r"\pi"),
-                                 min=min*10,
-                                 max=max*10),
-                      y=axis.lin(painter=ygridpainter))
+                      x=axis.lin(painter=gridpainter),
+                      y=axis.lin(painter=gridpainter))
 
-    g.plot(graph.data.function(func, points=puntos))
+    g.plot(graph.data.function(func, max=max, min=min))
 
     g.writePDFfile(nombre)
 
 
 
-def test():
+if __name__ == '__main__':
     g = graph.graphxy(width=8)
     # either provide lists of the individual coordinates
-    g.plot(graph.data.values(x=list(range(10)), y=list(range(10))))
+    g.plot(graph.data.function(func, points=puntos))
     # or provide one list containing the whole points
     g.plot(graph.data.points(list(zip(list(range(10)), list(range(10)))), x=1, y=2))
-    g.writeEPSfile("points")
     g.writePDFfile("points")
